34fb58519420ce39a72f7c63f4b51b750c5e6758
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 10:43:23 +0530

    list

--- List/CRUD.py
+++ List/CRUD.py
@@ -1,14 +1,28 @@
-list=[7,8293,7,-8,-2,89,-34,93,90]
-print(list[1:9:3])
-print(list[:-5:-10])
-# delete 
-# ADD ELEMENET 
-list.append("hellloo")
-print(list)
+# list=[7,8293,7,-8,-2,89,-34,93,90]
+# print(list[1:9:3])
+# print(list[:-5:-10])
+# # delete 
+# # ADD ELEMENET 
+# list.append("hellloo")
+# print(list)
 
-list.insert(3,10)
-print(list)
-list1=[78,9983,3784]
-# list.extend(list1)
-list+=list1   #extend 
-print(list)
+# list.insert(3,10)
+# print(list)
+# list1=[78,9983,3784]
+# # list.extend(list1)
+# list+=list1   #extend 
+# print(list)
+
+last=[5,3,2,1,4,8]
+total=sum(last)
+print(total)
+ans=[]
+for i in last:
+    ans.append(total-i)
+print(ans)
+
+# total=sum(last)
+# print(total)
+# for i in range(len(last)):
+#     print(last[total-i],end=" ")
+
