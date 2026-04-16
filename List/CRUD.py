32e066c32fee173a397fd89afe856bac2397fc59
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 11:02:13 +0530

    sum of list in list

--- List/CRUD.py
+++ List/CRUD.py
@@ -13,16 +13,43 @@
 # list+=list1   #extend 
 # print(list)
 
-last=[5,3,2,1,4,8]
-total=sum(last)
-print(total)
-ans=[]
-for i in last:
-    ans.append(total-i)
-print(ans)
+# last=[5,3,2,1,4,8]
+# total=sum(last)
+# print(total)
+# ans=[]
+# for i in last:
+#     ans.append(total-i)
+# print(ans)
+# ans=[18, 20, 21, 22, 19, 15]
 
 # total=sum(last)
 # print(total)
 # for i in range(len(last)):
 #     print(last[total-i],end=" ")
 
+# list=[[1,3,5,6],[4,5,7,4],[7,3,9,2],[8,47,73,7]]
+# d=list[0]
+# a=list[1]
+# b=list[2]
+# c=list[3]
+# print(sum(d))
+# print(sum(a))
+# print(sum(b))
+# print(sum(c))
+
+
+
+# # total=sum(list)
+# # print(total)
+
+# a=lst
+
+list=[[1,3,5,6],[4,5,7,4],[7,3,9,2],[8,47,73,7]]
+
+ans=[]
+for i in range(len(list)):
+    temp=0
+    for j in list[i]:
+        temp+=j
+    ans.append(temp)
+print(ans)
---
+++ List/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a=list[1]
+# b=list[2]
+# c=list[3]
+# d=list[4]
+
+
+# # total=sum(list)
+# # print(total)
+
+# a=lst
