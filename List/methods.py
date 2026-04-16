53a788242b87c97ae78df20c0e9bb8c7429cdb6e
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 12:56:20 +0530

    short list in asceding and deceding order

---
+++ List/methods.py
@@ -0,0 +1,9 @@
+# sorted 
+lst=[5,7,3,5,9,8,0,6,5,3,5]
+# lst.sort() # ascrding order 
+# lst.sort(reverse=True) #deceding order
+# print(lst)
+# lst1=sorted(lst)  #ascedng order
+print(lst)
+lst1=sorted(lst,reverse=True) #deceding order
+print(lst1)
