ca2b983ffbb3b890b0f0c2a698d16908b2557086
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 12:47:12 +0530

    clear llist

--- List/deleteList.py
+++ List/deleteList.py
@@ -1,6 +1,14 @@
 # /DELETE 
 lst=[4,6,4,3,3,5,7,4,2]
 print(lst)
+a=lst.count(4) #count num of occurance
+b=lst.index(2)  # value kaunse index par hea 
+print("count=",a)
+print("b=",b)
+
+# clear list
+lst.clear()
+print(lst)
 # del lst[5]
 # print(lst)
 # lst.pop(5) #delete with index
@@ -10,5 +18,7 @@ print(lst)
 # print(x)
 
 # value deletr 
-lst.remove(3)  #remove value
-print(lst)
+# lst.remove(3)  #remove value
+# print(lst)
+# # lst.discart(4) #/does not work
+# print(lst)
