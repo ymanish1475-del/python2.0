d4c301f81c92f32337307f68e8fb7784a3f8dc86
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 10:12:30 +0530

    extend list

---
+++ List/CRUD.py
@@ -0,0 +1,14 @@
+list=[7,8293,7,-8,-2,89,-34,93,90]
+print(list[1:9:3])
+print(list[:-5:-10])
+# delete 
+# ADD ELEMENET 
+list.append("hellloo")
+print(list)
+
+list.insert(3,10)
+print(list)
+list1=[78,9983,3784]
+# list.extend(list1)
+list+=list1   #extend 
+print(list)
