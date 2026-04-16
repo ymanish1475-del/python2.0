cdd35cec9bb7a01ae588e491f5ba94b80aa94682
ymanish1475-del <ymanish1475@gmail.com>
2026-04-16 12:32:31 +0530

    remove the value of element in list

---
+++ List/deleteList.py
@@ -0,0 +1,14 @@
+# /DELETE 
+lst=[4,6,4,3,3,5,7,4,2]
+print(lst)
+# del lst[5]
+# print(lst)
+# lst.pop(5) #delete with index
+
+# print(lst)
+# x=lst.pop(5)
+# print(x)
+
+# value deletr 
+lst.remove(3)  #remove value
+print(lst)
