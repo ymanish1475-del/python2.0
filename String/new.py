5385f4d62b43d0ba42985b83d3b4bbc04d73ff7d
ymanish1475-del <ymanish1475@gmail.com>
2026-04-28 10:12:48 +0530

    reverse the string

---
+++ String/new.py
@@ -0,0 +1,5 @@
+name="manish yadav";
+# name=name.uppercase();
+print(name.upper());
+name1=" brother";
+print(name+name1)
---
+++ String/reversePara.py
@@ -0,0 +1,13 @@
+p="lets learn the python";
+# pyhthon the learn lets
+
+p=p.split();
+print(p);
+p.reverse()
+p=" ".join(p).capitalize();
+print(p);
+
+
+# print(p);
+# # p=p.join();
+# print(p.join())
