ca5e781b86e4c7919106a80e2f74f8f696ad741f
ymanish1475-del <ymanish1475@gmail.com>
2026-05-05 09:51:05 +0530

    Q

---
+++ String/AssFindInQ3.py
@@ -0,0 +1,6 @@
+# finding the index
+# 3. Write a Python program that takes a string and a character as 
+# input and returns a list of indices where the character occurs in the 
+# string. 
+# Input: "programming", 'm' 
+# Output: [6, 7] 
---
+++ String/AssValidEmailQ1.py
@@ -0,0 +1,5 @@
+# valid email address
+email="rajeshpatel197422@gmail.com"
+
+i=email.index(7)
+print(i)
